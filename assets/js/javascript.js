95d545511c3a8154cd72e3993fa0b58fbf2d618a
Vitalij98 <vitali.mazzucchi2@gmail.com@gmail.com>
2022-05-09 17:09:59 +0200

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,19 @@
+
+
+var nome ='Inserisci il tuo nome';
+window.alert=nome
+var cognome ='Inserisci il tuo cognome';
+var sesso ='Inserisci il tuo sesso';
+var citta ='Inserisci la tua Citta';
+var email ='Inserisci il tuo indirizzo di posta eletytronica';
+var eta ='inserisci la tua eta';
+/* window.prompt(nome);
+window.prompt(cognome);
+window.prompt(sesso);
+window.prompt(eta);
+window.prompt(citta);
+window.prompt(email); */
+
+
+
+
